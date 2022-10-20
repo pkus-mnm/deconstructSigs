8dd17774039aa804f99d966fbf90d28e24493cf3
Paweł Kuś <pawel.kus@mnm.bio>
2022-10-20 10:21:57 +0200

    dbs_possible moved to internal data

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,3 +1,3 @@
 ## code to prepare `sysdata` dataset goes here
 
-usethis::use_data(tri.counts.exome, tri.counts.genome, internal = TRUE)
+usethis::use_data(tri.counts.exome, tri.counts.genome, dbs_possible, internal = TRUE, overwrite = TRUE)
